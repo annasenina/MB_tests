2a67203fb3d0b655c49740896371b529b1d2587f
Anna Senina <as.senina@gmail.com>
2021-06-02 18:18:05 +0300

    Автоматизируем ручной сценарий теста "Забыли пароль?"

--- src/test/java/tests/TestLoginPage.java
+++ src/test/java/tests/TestLoginPage.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
 import io.qameta.allure.Feature;
+
+import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
+import static com.codeborne.selenide.Selectors.byText;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -20,18 +23,25 @@ public class TestLoginPage extends TestBase{
     void canWatchPasswordPhoneTest(){
 
         step ("Открываем главную страницу", ()-> open("https://multibonus.ru/"));
-
-
     }
 
     @Test
-    //@AllureId("2966")
+    @AllureId("2966")
     @DisplayName("Можно посмотреть подсказку у поля \"Пароль\" ")
     void canWatchPasswordTooltipTest(){
 
         step ("Открываем главную страницу", ()-> open("https://multibonus.ru/"));
+    }
 
+    @Test
+    @AllureId("2968")
+    @DisplayName("При клике \"Забыли пароль\" открывается страница восстановления пароля\n ")
+    void canClickForgetPasswordButtonTest(){
+        step ("Открываем главную страницу", ()-> open("https://multibonus.ru/"));
 
+        step ("Нажимаем на кнопку \"Забыли пароль?\"", ()-> $(byText("Забыли пароль?")).click());
+
+        step ("Проверяем адрес страницы" );
     }
 
 }
